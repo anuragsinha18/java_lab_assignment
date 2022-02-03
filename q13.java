0e3560f6ca5ec8667076f75c1b4e008c0f5cbb7e
Anurag Sinha <anurag.sinha@publicissapient.com>
2022-02-03 23:03:30 +0530

    update 5

--- q13.java
+++ q13.java
@@ -7,6 +7,7 @@ class q13
     public static void main(String args[])  
     {  
         String str = new String("The quick brown fox jumps over the lazy dog");
+        
         System.out.println("Printing 12th index of string\n"+str.charAt(11));
         System.out.println("Contains is or not\n"+str.contains("is"));
         System.out.println("Adding to string\n "+str.concat(" and killed it."));
@@ -15,12 +16,17 @@ class q13
         System.out.println("Length of the string is \n"+str.length());
         System.out.println("Checking equality with The quick brown Fox jumps over the lazy Dog\n"+str.equals("The quick brown Fox jumps over the lazy Dog"));
         System.out.println("Replacing The with A\n "+str.replace("The", "A"));
+
         str = new String("The quick brown fox ,jumps over the lazy dog");
+
         String splitted[] = str.split(",");
+
         System.out.println("Split two animals\n");
+
         for (String s : splitted) {
             System.out.println(s);
         }
+
         System.out.println("Printing Animals names \tfox\tdog");
         System.out.println("Lowercase string\n"+str.toLowerCase());
         System.out.println("Uppercase string\n"+str.toUpperCase());
